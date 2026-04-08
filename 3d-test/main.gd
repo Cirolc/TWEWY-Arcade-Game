30ed7929a35f89ac06664d7078e036cb150e6cc3
Cirolc <cholciroleon@gmail.com>
2026-04-08 12:09:23 -0300

    Better BG and foreground + camera fix
    Proud of my work today.

--- 3d-test/main.gd
+++ 3d-test/main.gd
@@ -1,14 +1,37 @@
 extends Node
 
 var wait = 60
+var stop = false
+var x = 0
+var z = 0
 
 func _ready() -> void:
 	pass
 
 func _process(delta: float) -> void:
-	if wait == 0:
-		$Player/Camera3D.v_offset -= 0.1
-	else:
-		wait -= 1
+	if not stop:
+		if wait == 0:
+			$Player/Camera3D.v_offset -= 0.1
+		else:
+			wait -= 1
 	if $Player/Camera3D.v_offset < 0:
 		$Player/Camera3D.v_offset = 0
+		stop=true
+
+	x = $Player/Camera3D.global_position[0]
+	x = clamp(x,-3,3)
+	$Player/Camera3D.global_position[0] = x
+	if $Player.global_position[0] > 3 or $Player.global_position[0] < -3:
+		if x < 0:
+			x = -3
+		else:
+			x = 3
+	$Player/Camera3D.global_position[0] = x
+	z = $Player/Camera3D.global_position[2]
+	z = clamp(z,-5,8)
+	if $Player.global_position[2] > 1.5 or $Player.global_position[2] < -5:
+		if z < 0:
+			z = -5
+		else:
+			z = 8
+	$Player/Camera3D.global_position[2] = z
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # TWEWY Arcade Game
 A TWEWY fangame to the best of my ability.
 
-This game is being made for about 6 months (starting [March 31st]).
+This game is being made for about 6 months (starting [March 31st, 2026]).
 The game is far from complete.
