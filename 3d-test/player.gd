2e4b556690b85c6bac3b9a285c3cd57e739e240a
Cirolc <cholciroleon@gmail.com>
2026-04-12 16:54:09 -0300

    cleaning up and another (failed) attempt at a mouse control code.

--- 3d-test/player.gd
+++ 3d-test/player.gd
@@ -1,10 +1,9 @@
 extends CharacterBody3D
 
-@export var speed = 14
+@export var speed = 10
 @export var fall_acceleration = 75
 @export var jump_impulse = 20
 @export var bounce_impulse = 16
-@export var mi : CharacterBody3D
 
 var target_velocity = Vector3.ZERO
 var start = 234
@@ -14,29 +13,17 @@ func _input(event):
 		if not event.pressed and start == 0:
 			$AnimationPlayer.current_animation="Idle"
 
-func _physics_process(delta):
-	var direction = Vector3.ZERO
-	var mousePos = $Camera3D.get_viewport().get_mouse_position()
-	var ray = $Camera3D.project_ray_origin(mousePos)
-	var camdirection = $Camera3D.project_ray_normal(mousePos)
-	var plane = Plane(Vector3.UP)
-	var inters = plane.intersects_ray(ray, camdirection)
+var direction = Vector3.ZERO
 
+func _physics_process(delta):
+	var mousePos = get_viewport().get_mouse_position()
+	print(mousePos)
 	if start == 0:
-		if Input.is_action_pressed("Right"):
-			direction.x += 1
-			$AnimationPlayer.current_animation="Run"
-		if Input.is_action_pressed("Left"):
-			direction.x -= 1
-			$AnimationPlayer.current_animation="Run"
-		if Input.is_action_pressed("Down"):
-			direction.z += 1
-			$AnimationPlayer.current_animation="Run"
-		if Input.is_action_pressed("Up"):
-			direction.z -= 1
-			$AnimationPlayer.current_animation="Run"
-		if is_on_floor() and Input.is_action_just_pressed("Jump"):
-			target_velocity.y = jump_impulse
+		if direction:
+			velocity = -transform.basis.z * speed
+			if transform.origin.distance_to(direction) < .5:
+				direction = Vector3.ZERO
+				velocity = Vector3.ZERO
 
 		if direction.x < 0:
 			$Sprite3D.flip_h = false
@@ -55,16 +42,8 @@ func _physics_process(delta):
 
 		for index in range(get_slide_collision_count()):
 			var collision = get_slide_collision(index)
-
 			if collision.get_collider() == null:
 				continue
-
-			if collision.get_collider().is_in_group("mob"):
-				var mob = collision.get_collider()
-				if Vector3.UP.dot(collision.get_normal()) > 0.1:
-					mob.squash()
-					target_velocity.y = bounce_impulse
-					break
 	else:
 		start-=1
 		if start < 163:
