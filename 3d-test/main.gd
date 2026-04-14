59873000cae99cc9a2b6678e9c93e822a7df18a2
Cirolc <cholciroleon@gmail.com>
2026-04-14 15:26:03 -0300

    code rewrite

--- 3d-test/main.gd
+++ 3d-test/main.gd
@@ -11,27 +11,35 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	if not stop:
 		if wait == 0:
-			$Player/Camera3D.v_offset -= 0.1
+			$Camera3D.v_offset -= 0.1
 		else:
 			wait -= 1
-	if $Player/Camera3D.v_offset < 0:
-		$Player/Camera3D.v_offset = 0
+	if $Camera3D.v_offset < 0:
+		$Camera3D.v_offset = 0
 		stop=true
 
-	x = $Player/Camera3D.global_position[0]
+	x = $Camera3D.global_position[0]
 	x = clamp(x,-3,3)
-	$Player/Camera3D.global_position[0] = x
+	$Camera3D.global_position[0] = x
 	if $Player.global_position[0] > 3 or $Player.global_position[0] < -3:
 		if x < 0:
 			x = -3
 		else:
 			x = 3
-	$Player/Camera3D.global_position[0] = x
-	z = $Player/Camera3D.global_position[2]
+	$Camera3D.global_position[0] = x
+	z = $Camera3D.global_position[2]
 	z = clamp(z,-5,8)
 	if $Player.global_position[2] > 1.5 or $Player.global_position[2] < -5:
 		if z < 0:
 			z = -5
 		else:
 			z = 8
-	$Player/Camera3D.global_position[2] = z
+	$Camera3D.global_position[2] = z
+
+func _on_floor_input_event(camera, event, click_position, click_normal, shape_idx) -> void:
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT):
+		$Marker.transform.origin = click_position
+		$Marker.transform.origin[0] = clamp($Marker.transform.origin[0], -10, 10)
+		$Marker.transform.origin[2] = clamp($Marker.transform.origin[2], -5, 4)
+		click_position = ($Marker.transform.origin)
+		$Player.direction = click_position
--- 3d-test/player.gd
+++ 3d-test/player.gd
@@ -5,7 +5,6 @@ extends CharacterBody3D
 @export var jump_impulse = 20
 @export var bounce_impulse = 16
 
-var target_velocity = Vector3.ZERO
 var start = 234
 
 func _input(event):
@@ -16,23 +15,13 @@ func _input(event):
 var direction = Vector3.ZERO
 
 func _physics_process(delta):
-	var mousePos = get_viewport().get_mouse_position()
-	print(mousePos)
-	var rayOrg:Vector3 = $Camera3D.project_ray_origin(mousePos)
-	var rayNor:Vector3 = $Camera3D.project_ray_normal(mousePos)
-	var Inters = Plane(Vector3.UP).intersects_ray(rayOrg,rayNor)
 	if start == 0:
-		if Inters and Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT):
-			direction = Inters.x
-			global_position.z = Inters.z
-			global_position.x = clamp(global_position.x,-10,10)
-			global_position.z = clamp(global_position.z,-5,4)
-			$Marker.global_position.x = Inters.x
-			$Marker.global_position.z = Inters.z
-			$Marker.global_position.x = clamp(global_position.x,-10,10)
-			$Marker.global_position.z = clamp(global_position.z,-5,4)
-
+		velocity.y -= fall_acceleration * delta
 		if direction:
+			look_at(direction, Vector3.UP)
+			rotation.x = 0
+			$Sprite3D.rotation.y = -rotation.y
+			$Camera3D.rotation.y = -rotation.y
 			velocity = -transform.basis.z * speed
 			if transform.origin.distance_to(direction) < .5:
 				direction = Vector3.ZERO
@@ -42,16 +31,6 @@ func _physics_process(delta):
 			$Sprite3D.flip_h = false
 		elif direction.x > 0:
 			$Sprite3D.flip_h = true
-
-		if direction != Vector3.ZERO:
-			direction = direction.normalized()
-			$Pivot.basis = Basis.looking_at(direction)
-
-		target_velocity.x = direction.x * speed
-		target_velocity.z = direction.z * speed
-
-		if not is_on_floor():
-			target_velocity.y = target_velocity.y - (fall_acceleration * delta)
 
 		for index in range(get_slide_collision_count()):
 			var collision = get_slide_collision(index)
@@ -63,5 +42,6 @@ func _physics_process(delta):
 			$AnimationPlayer.current_animation="Start"
 		if start == 0:
 			$AnimationPlayer.current_animation="Idle"
-	velocity = target_velocity
+	global_position.x = clamp(global_position.x, -10, 10)
+	global_position.z = clamp(global_position.z, -5, 4)
 	move_and_slide()
